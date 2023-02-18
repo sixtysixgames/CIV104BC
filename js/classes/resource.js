b976930a2749be54bce545619fab11dcb93d6ede
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-02-18 12:29:53 +0000

    use internal property for .net because my saved game managed to get a NaN value;  remove commented code

--- js/classes/resource.js
+++ js/classes/resource.js
@@ -13,28 +13,43 @@ Resource.prototype = new CivObj({
     constructor: Resource,
     type: civObjType.resource,
     // 'net' accessor always exists, even if the underlying value is undefined for most resources.
+    //get net() {
+    //    if (typeof this.data.net != "number") {
+    //        console.warn(this.id + ".net get not a number: " + this.data.net);
+    //        //return 0;
+    //        this.data.net = 0; // hack to fix NaN stored in .net
+    //    }
+    //    return this.data.net;
+    //},
+    //set net(value) {
+    //    if (typeof value != "number") {
+    //        console.warn(this.id + ".net set value not a number: " + value);
+    //        //value = 0; // hack to fix NaN stored in .net
+    //        //this.data.net = 0;
+    //    }
+    //    else {
+    //        this.data.net = value;
+    //    }
+    //},
     get net() {
-        if (typeof this.data.net != "number") {
-            console.warn(this.id + ".net get not a number: " + this.data.net);
-            //return 0;
-            this.data.net = 0; // hack to fix NaN stored in .net
+        if (typeof this.nett != "number") {
+            console.warn(this.id + ".nett get not a number: " + this.nett);
+            this.nett = 0; // hack to fix NaN stored in .net
         }
-        return this.data.net;
+        return this.nett;
     },
     set net(value) {
         if (typeof value != "number") {
             console.warn(this.id + ".net set value not a number: " + value);
-            //value = 0; // hack to fix NaN stored in .net
-            //this.data.net = 0;
         }
         else {
-            this.data.net = value;
+            this.nett = value;
         }
     },
     increment: 0,
     specialChance: 0,
     specialMaterial: "",
-    activity: "gathering", //I18N
+    activity: "gathering",
     // for variable trading costs
     get tradeAmount() { return this.data.tradeAmount; },
     set tradeAmount(value) { this.data.tradeAmount = value; }
