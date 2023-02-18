83e4f80bfb3a583ed6744a4464f3d49fb6917377
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-02-18 13:57:39 +0000

    added efficiency for use with enemy soldiers when raiding

--- js/data/civ-sizes.js
+++ js/data/civ-sizes.js
@@ -1,20 +1,21 @@
 // Civ size category minimums
+// efficiency: used to increase efficiency of defending enemy troops
 const civSizes = [
-    { min_pop: 0, name: "Tribe", id: "tribe" },
-    { min_pop: 10, name: "Thorp", id: "thorp" },
-    { min_pop: 50, name: "Hamlet", id: "hamlet" },
-    { min_pop: 100, name: "Village", id: "village" },
-    { min_pop: 500, name: "Small Town", id: "smallTown" },
-    { min_pop: 1000, name: "Town", id: "town" },
-    { min_pop: 2500, name: "Large Town", id: "largeTown" },
-    { min_pop: 5000, name: "Small City", id: "smallCity" },
-    { min_pop: 10000, name: "City", id: "city" },
-    { min_pop: 25000, name: "Large City", id: "largeCity" },
-    { min_pop: 50000, name: "City State", id: "metropolis" },
-    { min_pop: 100000, name: "Small Nation", id: "smallNation" },
-    { min_pop: 250000, name: "Nation", id: "nation" },
-    { min_pop: 500000, name: "Large Nation", id: "largeNation" },
-    { min_pop: 1000000, name: "Empire", id: "empire" }
+    { min_pop: 0, name: "Tribe", id: "tribe", efficiency: 0.005 },
+    { min_pop: 10, name: "Thorp", id: "thorp", efficiency: 0.006 },
+    { min_pop: 50, name: "Hamlet", id: "hamlet", efficiency: 0.008 },
+    { min_pop: 100, name: "Village", id: "village", efficiency: 0.011 },
+    { min_pop: 500, name: "Small Town", id: "smallTown", efficiency: 0.015 },
+    { min_pop: 1000, name: "Town", id: "town", efficiency: 0.021 },
+    { min_pop: 2500, name: "Large Town", id: "largeTown", efficiency: 0.028 },
+    { min_pop: 5000, name: "Small City", id: "smallCity", efficiency: 0.036 },
+    { min_pop: 10000, name: "City", id: "city", efficiency: 0.045 },
+    { min_pop: 25000, name: "Large City", id: "largeCity", efficiency: 0.055 },
+    { min_pop: 50000, name: "City State", id: "metropolis", efficiency: 0.066 },
+    { min_pop: 100000, name: "Small Nation", id: "smallNation", efficiency: 0.078 },
+    { min_pop: 250000, name: "Nation", id: "nation", efficiency: 0.091 },
+    { min_pop: 500000, name: "Large Nation", id: "largeNation", efficiency: 0.105 },
+    { min_pop: 1000000, name: "Empire", id: "empire", efficiency: 0.12 }
 ];
 
 export { civSizes };
