8fc1d02e328628f30c0fc0c0ca3823ca9012e777
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-02-18 13:58:03 +0000

    added lead tin silver

--- js/data/lists.js
+++ js/data/lists.js
@@ -4,7 +4,7 @@ import { unitType } from "../index.js";
 const PATIENT_LIST = [
     unitType.healer, unitType.cleric, unitType.farmer, unitType.soldier, unitType.cavalry, unitType.labourer,
     unitType.woodcutter, unitType.miner, unitType.tanner, unitType.blacksmith,
-    unitType.charBurner, unitType.ironsmith, unitType.coppsmith,
+    unitType.charBurner, unitType.ironsmith, unitType.coppsmith, unitType.leadsmith, unitType.tinsmith, unitType.silversmith,
     unitType.unemployed
 ];
 export { PATIENT_LIST };
