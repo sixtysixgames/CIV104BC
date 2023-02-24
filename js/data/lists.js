e0e008649b8e879a7f84473c7efc886d326b54aa
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-02-24 18:54:10 +0000

    added lime burner patient

--- js/data/lists.js
+++ js/data/lists.js
@@ -3,7 +3,7 @@ import { unitType } from "../index.js";
 //most important is first in list
 const PATIENT_LIST = [
     unitType.healer, unitType.cleric, unitType.farmer, unitType.soldier, unitType.cavalry, unitType.labourer,
-    unitType.woodcutter, unitType.miner, unitType.tanner, unitType.blacksmith,
+    unitType.woodcutter, unitType.miner, unitType.tanner, unitType.blacksmith, unitType.limeBurner,
     unitType.charBurner, unitType.ironsmith, unitType.coppsmith, unitType.leadsmith, unitType.tinsmith, unitType.silvsmith,
     unitType.unemployed
 ];
