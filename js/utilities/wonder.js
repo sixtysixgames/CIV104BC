a5f7acc885636ad4487233a5b631d4c0238cf6fb
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-04-21 17:58:59 +0000

    change gold to coins

--- js/utilities/wonder.js
+++ js/utilities/wonder.js
@@ -60,8 +60,8 @@ function getWonderCostMultiplier() { // Based on the most wonders in any single 
 }
 
 function speedWonder() {
-    if (civData.gold.owned < 100) { return; }
-    civData.gold.owned -= 100;
+    if (civData.coins.owned < 10000) { return; }
+    civData.coins.owned -= 10000;
     curCiv.curWonder.progress += 1 / getWonderCostMultiplier();
     curCiv.curWonder.rushed = true;
     updateWonder();
