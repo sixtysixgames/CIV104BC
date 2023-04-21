3f38cb74f5415e5ea875fabad80868db85e4d195
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-04-21 17:48:41 +0000

    new unit types

--- js/data/lists.js
+++ js/data/lists.js
@@ -3,8 +3,8 @@ import { unitType } from "../index.js";
 //most important is first in list
 const PATIENT_LIST = [
     unitType.healer, unitType.cleric, unitType.farmer, unitType.soldier, unitType.cavalry, unitType.labourer,
-    unitType.woodcutter, unitType.miner, unitType.tanner, unitType.blacksmith, unitType.limeBurner,
-    unitType.charBurner, unitType.ironsmith, unitType.coppsmith, unitType.leadsmith, unitType.tinsmith, unitType.silvsmith,
+    unitType.woodcutter, unitType.miner, unitType.tanner, unitType.blacksmith, unitType.limeBurner, unitType.charBurner,
+    unitType.ironsmith, unitType.coppsmith, unitType.leadsmith, unitType.tinsmith, unitType.silvsmith, unitType.mercsmith, unitType.goldsmith,
     unitType.unemployed
 ];
 export { PATIENT_LIST };
