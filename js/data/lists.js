7b0f657286f479ab6302b5401ba51e84a5bd5bd4
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-05-12 18:06:48 +0000

    added crop farmer, tree farmer, herb gardener, quarryer, ore miner

--- js/data/lists.js
+++ js/data/lists.js
@@ -1,10 +1,13 @@
 import { unitType } from "../index.js";
 
 //most important is first in list
+// i.e. we need healers to heal the sick then farmers to feed everyone then clerics to bury the dead to stop the plage spreading
+// then military to defend, then whatever
 const PATIENT_LIST = [
-    unitType.healer, unitType.cleric, unitType.farmer, unitType.soldier, unitType.cavalry, unitType.labourer,
+    unitType.healer, unitType.farmer, unitType.cropFarmer, unitType.herbGardener, unitType.cleric, unitType.soldier, unitType.cavalry, 
+    unitType.forester, unitType.quarWorker, unitType.oreMiner,
     unitType.woodcutter, unitType.miner, unitType.tanner, unitType.blacksmith, unitType.limeBurner, unitType.charBurner,
     unitType.ironsmith, unitType.coppsmith, unitType.leadsmith, unitType.tinsmith, unitType.silvsmith, unitType.mercsmith, unitType.goldsmith,
-    unitType.unemployed
+    unitType.labourer, unitType.unemployed
 ];
 export { PATIENT_LIST };
