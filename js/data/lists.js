3e0f03d1a10058da8f36e3735599bea1aed6d4a5
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-05-20 11:31:12 +0000

    new units meat and crop farmer

--- js/data/lists.js
+++ js/data/lists.js
@@ -4,7 +4,7 @@ import { unitType } from "../index.js";
 // i.e. we need healers to heal the sick then farmers to feed everyone then clerics to bury the dead to stop the plage spreading
 // then military to defend, then whatever
 const PATIENT_LIST = [
-    unitType.healer, unitType.farmer, unitType.cropFarmer, unitType.herbGardener, unitType.cleric, unitType.soldier, unitType.cavalry, 
+    unitType.healer, unitType.farmer, unitType.meatFarmer, unitType.cropFarmer, unitType.herbGardener, unitType.cleric, unitType.soldier, unitType.cavalry, 
     unitType.forester, unitType.quarWorker, unitType.oreMiner,
     unitType.woodcutter, unitType.miner, unitType.tanner, unitType.blacksmith, unitType.limeBurner, unitType.charBurner,
     unitType.ironsmith, unitType.coppsmith, unitType.leadsmith, unitType.tinsmith, unitType.silvsmith, unitType.mercsmith, unitType.goldsmith,
