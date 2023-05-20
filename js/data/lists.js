6c12ce0394c0f390526c59a4a9c0a652e2526108
sixtysixgames <nosbig.kram@hotmail.co.uk>
2023-05-20 12:54:17 +0000

    building list for conquering

--- js/data/lists.js
+++ js/data/lists.js
@@ -1,4 +1,5 @@
-import { unitType } from "../index.js";
+"use strict";
+import { unitType, buildingType } from "../index.js";
 
 //most important is first in list
 // i.e. we need healers to heal the sick then farmers to feed everyone then clerics to bury the dead to stop the plage spreading
@@ -10,4 +11,15 @@ const PATIENT_LIST = [
     unitType.ironsmith, unitType.coppsmith, unitType.leadsmith, unitType.tinsmith, unitType.silvsmith, unitType.mercsmith, unitType.goldsmith,
     unitType.labourer, unitType.unemployed
 ];
-export { PATIENT_LIST };
+
+// most useful first ie storage, then most useful work places.  Later ones will have fewer conquests
+// used when conquering neighbouring nations.  See utilities/combat.js > invade()
+const BUILDING_LIST = [
+    buildingType.barn, buildingType.woodstock, buildingType.stonestock, buildingType.skinShed, buildingType.herbShed, buildingType.oreShed,
+    buildingType.apothecary, buildingType.tannery, buildingType.smithy, buildingType.charKiln, buildingType.limeKiln,
+    buildingType.temple, buildingType.tent, buildingType.hut, buildingType.cottage, buildingType.tenement, buildingType.barracks, buildingType.stable,
+    buildingType.ironWorks, buildingType.coppWorks, buildingType.leadWorks, buildingType.tinWorks, buildingType.silvWorks, buildingType.mercWorks, buildingType.goldWorks,
+    buildingType.meatFarm, buildingType.cropFarm, buildingType.treeFarm, buildingType.herbGarden, buildingType.quarry, buildingType.oreMine,
+    buildingType.mansion, buildingType.palace
+];
+export { PATIENT_LIST, BUILDING_LIST };
